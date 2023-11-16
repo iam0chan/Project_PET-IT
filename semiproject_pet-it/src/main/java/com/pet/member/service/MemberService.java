4affbb52159bce32ae6818dcb670d3aa50b381fa
sidty <sidty@DESKTOP-GSF0T8P>
2023-11-16 17:45:42 +0900

    ty _ jm service수정

--- semiproject_pet-it/src/main/java/com/pet/member/service/MemberService.java
+++ semiproject_pet-it/src/main/java/com/pet/member/service/MemberService.java
@@ -11,12 +11,21 @@ import com.pet.member.dto.Member;
 public class MemberService {
 
 private MemberDao dao = new MemberDao();
-	
-	public Member selectMemberByIdAndPw(String memberId, String memberPw) {
-		Connection conn = getConnection();
-		Member m = dao.selectMemberByIdAndPw(conn, memberId, memberPw);
-		close(conn);
-		return m;
-	}
+   
+   public Member selectMemberByIdAndPw(String memberId, String memberPw) {
+      Connection conn = getConnection();
+      Member m = dao.selectMemberByIdAndPw(conn, memberId, memberPw);
+      close(conn);
+      return m;
+   }
 
+   public int insertMember(Member m) {
+      Connection conn = getConnection();
+      int result=dao.insertMember(conn, m);
+      if(result>0) commit(conn);
+      else rollback(conn);
+      close(conn);
+      
+      return result;
+   }
 }
