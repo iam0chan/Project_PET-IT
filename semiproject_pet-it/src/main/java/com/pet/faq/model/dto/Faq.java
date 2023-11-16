0ad42e24f7ceeb4df8a8ea9708e9cafe2a83c4ca
masai <masai@DESKTOP-NI5P0F3>
2023-11-16 18:52:02 +0900

    jh_충돌수정중

--- semiproject_pet-it/src/main/java/com/pet/faq/model/dto/Faq.java
+++ semiproject_pet-it/src/main/java/com/pet/faq/model/dto/Faq.java
@@ -14,21 +14,5 @@ public class Faq {
 	private String faqContent;
 
 }
-=======
-package com.pet.faq.model.dto;
 
-import lombok.Builder;
-import lombok.Data;
-
-@Data
-@Builder
-public class Faq {
-
-	private String faqNo;
-	private String faqCategory;
-	private String faqTitle;
-	private int faqHits;
-	private String faqContent;
-
-}
 
