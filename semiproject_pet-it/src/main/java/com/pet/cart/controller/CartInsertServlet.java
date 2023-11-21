b2121f04e6e4211e950cc7c97f624598d8221fc5
young <young@DESKTOP-0KP38OB>
2023-11-21 20:36:44 +0900

    dy)CartInsertServlet 추가

--- semiproject_pet-it/src/main/java/com/pet/cart/controller/CartInsertServlet.java
+++ semiproject_pet-it/src/main/java/com/pet/cart/controller/CartInsertServlet.java
@@ -7,35 +7,48 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- * Servlet implementation class CartInsertServlet
- */
+import com.pet.cart.model.dao.CartDao;
+import com.pet.cart.model.dto.Cart;
+import com.pet.product.model.dto.Product;
+
 @WebServlet("/CartInsertServlet")
 public class CartInsertServlet extends HttpServlet {
-	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
+    private static final long serialVersionUID = 1L;
+
     public CartInsertServlet() {
         super();
-        // TODO Auto-generated constructor stub
     }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
-	}
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        try {
+            // 사용자가 선택한 상품 정보를 받아오기
+            String productNo = request.getParameter("productNo");
+            String memberId = request.getParameter("memberId");
+            int cartProductCount = Integer.parseInt(request.getParameter("cartProductCount"));
+            String optionName = request.getParameter("optionName");
+            String optionValue = request.getParameter("optionValue");
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
-	}
+            // Cart 객체를 생성하여 정보 설정
+            Cart cart = Cart.builder()
+                    .productNo(productNo)
+                    .memberId(memberId)
+                    .cartProductCount(cartProductCount)
+                    .optionName(optionName)
+                    .optionValue(optionValue)
+                    .build();
 
+            // CartDao를 통해 장바구니에 상품 추가
+            CartDao cartDao = new CartDao();
+            cartDao.insertCart(request, cart);
+
+            // 장바구니 페이지로 이동합니다.
+            response.sendRedirect(request.getContextPath() + "/views/cart/cartList.jsp");
+
+        } catch (NumberFormatException e) {
+            // 예외 처리 코드 추가
+            e.printStackTrace(); // 또는 로깅 등의 작업을 수행할 수 있습니다.
+            response.sendError(HttpServletResponse.SC_BAD_REQUEST, "Invalid cartProductCount");
+        }
+    }
 }
