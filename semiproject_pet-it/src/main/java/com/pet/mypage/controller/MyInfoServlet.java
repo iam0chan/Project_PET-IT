2a38209f2ff968350b94d31557eb57b736cdcc07
young <young@DESKTOP-0KP38OB>
2023-11-22 09:20:27 +0900

    dy) 마이페이지 수정

--- semiproject_pet-it/src/main/java/com/pet/mypage/controller/MyInfoServlet.java
+++ semiproject_pet-it/src/main/java/com/pet/mypage/controller/MyInfoServlet.java
@@ -12,38 +12,23 @@ import javax.servlet.http.HttpSession;
 import com.pet.member.dto.Member;
 import com.pet.mypage.service.MyInfoService;
 
-/**
- * Servlet implementation class myInfo
- */
 @WebServlet("/myInfo.do")
 public class MyInfoServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-	/**
-     * @see HttpServlet#HttpServlet()
-     */
-    public MyInfoServlet() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-	
+	public MyInfoServlet() {
+		super();
+	}
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
 		request.getRequestDispatcher("/views/mypage/myInfo.jsp").forward(request, response);
 	}
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		doGet(request, response);
 	}
 
 }
-
